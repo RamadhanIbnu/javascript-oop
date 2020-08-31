4c34b201bc027785cb78a6e23ba17b231b855360
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-08-31 13:29:33 +0700

    adding class hero

--- exercise.js
+++ exercise.js
@@ -7,12 +7,13 @@ class Enemy{
     }
 
     attack(){
-        this.hp - 50;
+        this.hp -= 50;
+        return this.hp;
     }
 
     skillAttack(){
-        this.hp - 100;
-        this.mp -20
+        this.hp -= 100;
+        this.mp -=20
     }
 
 }
@@ -23,11 +24,11 @@ class Zombie extends Enemy {
     }
 
     walk(){
-        this.speed * 1;
+        this.speed *= 1;
     }
 
     running(){
-        this.speed * 5
+        this.speed *= 5
     }
 }
 
@@ -37,18 +38,32 @@ class Cerberus extends Enemy{
     }
 
     clawsAttack(){
-        this.hp - 100;
+        this.hp -= 100;
     }
 
     skillRoaring(){
-        this.hp - 500;
-        this.mp - 50;
+        this.hp -= 500;
+        this.mp -= 50;
     }
 
     enhanceAttack(){
-        this.atk * 2;
-        this.hp - 200;
-        this.mp - 10;
+        this.atk *= 2;
+        this.mp -= 10;
     }
 
+}
+
+
+
+class hero{
+    constructor(healthPoint, manaPoint, normalAttack, skillAttack){
+        this.hp = healthPoint;
+        this.mp = manaPoint;
+        this.atk = normalAttack;
+        this.skill = skillAttack;
+    }
+
+    attack(){
+
+    }
 }
